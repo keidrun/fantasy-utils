901a6d80dae6924585270b8eeec1cb446380b7d8
keidrun <kssk96@gmail.com>
2017-11-22 18:36:50 +0900

    Add example.

---
+++ example/examples.js
@@ -0,0 +1,12 @@
+const R = require('ramda');
+const F = require('fantasy-utils');
+
+const dashrize = R.compose(
+  R.join('-'),
+  R.map(R.toLower),
+  F.trace('debug:'), // print middle data for debugging
+  R.split(' '),
+  R.replace(/\s{2,}/gi, '')
+);
+
+console.log('result:', dashrize('Do you know our world is small?'));
